829312397b2673ba30be7a7963b56c8bb04b8971
shabnam <shabnam@DESKTOP-I8QVSKK>
2017-08-29 11:20:48 +0430

    Merge origin/master
    Conflicts:
    	reports/ui/WritsDetailByDateReport.php

--- accounting/cheque/cheques.data.php
+++ accounting/cheque/cheques.data.php
@@ -480,6 +480,14 @@ function editCheque(){
 	
 	ACC_IncomeCheques::AddToHistory($obj->IncomeChequeID, $obj->IncomeChequeID, $pdo, $_POST["reason"]);
 	
+	$BackPays = $obj->GetBackPays($pdo);
+	if(count($BackPays) > 0)
+	{
+		$bobj = new LON_BackPays($BackPays[0]["BackPayID"]);
+		$bobj->PayAmount = $_POST["newAmount"];
+		$bobj->Edit($pdo);
+	}
+	
 	if(ExceptionHandler::GetExceptionCount() > 0)
 	{
 		$pdo->rollBack();
