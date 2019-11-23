033d654c173bcf6e42206c1d98e4e8e192327044
mahdipour <mahdipour@DESKTOP-UJ94VJ3>
2019-11-23 10:39:50 +0330

    Merge origin/master
    Conflicts:
    	gateway/DataSummary.class.php

--- messenger/ManageGroup.data.php
+++ messenger/ManageGroup.data.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../header.inc.php';
 require_once 'ManageGroup.class.php';
-//require_once '../class/study_branch.class.php';
+require_once 'definitions.inc.php';
 require_once(inc_response);
 require_once inc_dataReader;
 require_once inc_PDODataAccess;
@@ -37,7 +37,18 @@ switch ( $task) {
         
     case "SaveMsg" :
           SaveMsg();
-	      
+    
+    case "SearchMsg" :
+          SearchMsg();
+        
+    case "DelMsg" :
+          DelMsg();
+        
+    case "GetNotNumber" :
+        GetNotNumber();
+        
+    case "SeenMsg" : 
+        SeenMsg();
 }
 
 function SelectGrop()
@@ -97,7 +108,108 @@ function SelectGrop()
 
 function SaveGrp()
 {
-	$obj = new manage_msg_group();
+    
+    //.................. secure section .....................
+    if (!InputValidation::validate($_REQUEST['GID'], InputValidation::Pattern_Num, false)) {
+            echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+            die();
+    }
+    
+    $obj = new manage_msg_group();
+    PdoDataAccess::FillObjectByArray($obj, $_POST);
+    
+    if (empty($obj->GID)) {
+
+        $size = $_FILES['FileType']['size'];
+
+        if ($size > 2097152) { 
+            echo '<script language="javascript">';
+            echo 'alert("حجم فایل باید کمتراز 2 مگابایت باشد.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        }
+        $st = preg_split("/\./", $_FILES ['FileType'] ['name']);
+
+        $extension = $st [count($st) - 1];
+
+        if (in_array($extension, array("jpg", "jpeg", "gif", "png", "pdf", "JPG", "JPEG", "GIF", "PNG", "PDF")) === false) {           
+            echo '<script language="javascript">';
+            echo 'alert("فرمت فایل غیرمجاز است.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        } else {
+         
+            $filetype = $obj->FileType = $extension; 
+            $return = $obj->Add();    
+               
+            $filename = $obj->GID;
+
+            if (file_exists(GRPPIC_DIRECTORY . $filename . "." . $filetype)) {
+                unlink(GRPPIC_DIRECTORY . $filename . "." . $filetype);
+            }
+                
+            $fp = fopen(GRPPIC_DIRECTORY . $filename . "." . $filetype, "w");
+            fwrite($fp, fread(fopen($_FILES['FileType']['tmp_name'], 'r'), $_FILES['FileType']['size']));
+            fclose($fp);
+            
+        }
+    } 
+    else {
+        
+        if(!empty($_FILES ['FileType'] ['name']))
+        {
+        $size = $_FILES['FileType']['size'];
+
+        if ($size > 2097152) { 
+            echo '<script language="javascript">';
+            echo 'alert("حجم فایل باید کمتراز 2 مگابایت باشد.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        }
+        $st = preg_split("/\./", $_FILES ['FileType'] ['name']);
+
+        $extension = $st [count($st) - 1];
+
+        if (in_array($extension, array("jpg", "jpeg", "gif", "png", "pdf", "JPG", "JPEG", "GIF", "PNG", "PDF")) === false) {           
+            echo '<script language="javascript">';
+            echo 'alert("فرمت فایل غیرمجاز است.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        } 
+        
+        $filetype = $obj->FileType = $extension; 
+        $return = $obj->Edit();                   
+        $filename = $obj->GID;
+
+        if (file_exists(GRPPIC_DIRECTORY . $filename . "." . $filetype)) {
+            unlink(GRPPIC_DIRECTORY . $filename . "." . $filetype);
+        }
+
+        $fp = fopen(GRPPIC_DIRECTORY . $filename . "." . $filetype, "w");
+        fwrite($fp, fread(fopen($_FILES['FileType']['tmp_name'], 'r'), $_FILES['FileType']['size']));
+        fclose($fp);
+            
+        
+        } else {                     
+            $return = $obj->Edit();                   
+            $filename = $obj->GID;            
+        }
+        
+    }
+    
+    if (!$return) {
+        echo Response::createObjectiveResponse($return, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+    echo Response::createObjectiveResponse(true, "");
+    die();
+    
+    //......................
+	/*$obj = new manage_msg_group();
     PdoDataAccess::FillObjectByJsonData($obj, $_POST["record"]);
 	
 	if(empty($obj->GID))
@@ -110,7 +222,7 @@ function SaveGrp()
 		die();
 	}
 	echo Response::createObjectiveResponse(true, "");
-	die();
+	die();*/
 		
 }
 
@@ -210,7 +322,7 @@ function SelectMyMessage()
     $where = "" ; 
     $whereParam = array(":PID" => $_SESSION["USER"]["PersonID"] );
     
-	$temp = manage_msg_members::GetAllMyMessage($where,$whereParam);
+	$temp = manage_msg_messages::GetAllMyMessage($where,$whereParam);
  
 	$no = count($temp);
         //..........................secure section ........................
@@ -229,47 +341,235 @@ function SelectMyMessage()
 }
 
 function SelectMessageGrp()
-{    
+{           
     $where = "" ; 
-    $whereParam = array(); array(":PID" => $_SESSION["USER"]["PersonID"] );
-    
-	$temp = manage_msg_members::GetAllGroupMessage($where,$whereParam);
-
+    $whereParam = array(":GID" => $_REQUEST['GID']);
+  
+	$temp = manage_msg_messages::GetAllGroupMessage($where,$whereParam);     
 	$no = count($temp);
-        //..........................secure section ........................
-        $start = (int)$_GET["start"] ;
-        $limit = (int)$_GET["limit"] ;
-        if(!InputValidation::validate($_GET["callback"], InputValidation::Pattern_EnAlphaNum, false))
-        {
-            echo dataReader::getJsonData(array(), 0);
-            die();
-        }
-        //................................................................
-        $temp = array_slice($temp,$start,$limit);
+    
+    //..........................secure section ........................
+    $start = (int)$_GET["start"] ;
+    $limit = (int)$_GET["limit"] ;
+    if(!InputValidation::validate($_GET["callback"], InputValidation::Pattern_EnAlphaNum, false))
+    {
+        echo dataReader::getJsonData(array(), 0);
+        die();
+    }
+    //................................................................
+    $temp = array_slice($temp,$start,$limit);
 	
 	echo dataReader::getJsonData ($temp, $no, $_GET ["callback"] );
 	die ();
 }
 
+function SearchMsg()
+{        
+    $where = "" ; 
+    $whereParam = array(":PID" => $_SESSION["USER"]["PersonID"],":GID" => $_REQUEST['GID'],":STxt" => "%".$_REQUEST['SearchTxt']."%" );
+    
+	$temp = manage_msg_messages::GetSearchMessage($where,$whereParam);
+    
+	$no = count($temp);
+    //..........................secure section ........................
+    $start = (int)$_GET["start"] ;
+    $limit = (int)$_GET["limit"] ;
+    if(!InputValidation::validate($_GET["callback"], InputValidation::Pattern_EnAlphaNum, false))
+    {
+        echo dataReader::getJsonData(array(), 0);
+        die();
+    }
+    //................................................................
+    $temp = array_slice($temp,$start,$limit);
+	
+	echo dataReader::getJsonData ($temp, $no, $_GET ["callback"] );
+	die ();
+}
+
+
 function SaveMsg()
 {
-	echo "*********"; die();
-    /*$obj = new manage_msg_group();
-    PdoDataAccess::FillObjectByJsonData($obj, $_POST["record"]);
-	
-	if(empty($obj->GID))
-		$return = $obj->Add();
-	else
-		$return = $obj->Edit();
-	if(!$return)
-	{
-		echo Response::createObjectiveResponse($return, ExceptionHandler::GetExceptionsToString());
-		die();
-	}
-	echo Response::createObjectiveResponse(true, "");
-	die();*/
+	//.................. secure section .....................
+    if (!InputValidation::validate($_REQUEST['MsgTxt'], InputValidation::Pattern_FaEnAlphaNum, false)) {
+        echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+    if (!InputValidation::validate($_REQUEST['ParentMSGID'], InputValidation::Pattern_Num, false)) {
+        echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+    if (!InputValidation::validate($_REQUEST['GID'], InputValidation::Pattern_Num, false)) {
+        echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+    
+    if (!InputValidation::validate($_REQUEST['MID'], InputValidation::Pattern_Num, false)) {
+        echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+        
+    $obj = new manage_msg_messages();    
+    $obj->GID = $_REQUEST['GID'] ; 
+    $obj->MID = $_REQUEST['MID'] ; 
+    $obj->ParentMSGID = $_REQUEST['ParentMSGID'] ; 
+    $obj->MSGID =  $_REQUEST['MSGID'] ; 
+    $obj->message = (empty($_REQUEST['MsgTxt']) ? " " : $_REQUEST['MsgTxt'] ) ;
+    $obj->SendingDate = DateModules::NowDateTime() ; 
+       
+    if (empty($obj->MSGID) && !($obj->MSGID > 0) ) {
+        
+        echo "**111**";
+        die();
+        
+        $size = $_FILES['FileType']['size'];
+
+        if ($size > 2097152) { 
+            echo '<script language="javascript">';
+            echo 'alert("حجم فایل باید کمتراز 2 مگابایت باشد.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        }
+        $st = preg_split("/\./", $_FILES ['FileType'] ['name']);
+
+        $extension = $st [count($st) - 1];
+
+        if (!empty($_FILES ['FileType'] ['name']) && in_array($extension, array("jpg", "jpeg", "gif", "png", "pdf","txt","docx", "JPG", "JPEG", "GIF", "PNG", "PDF","TXT","DOCX")) === false) {           
+            echo '<script language="javascript">';
+            echo 'alert("فرمت فایل غیرمجاز است.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        } else {
+         
+            $filetype = $obj->FileType = $extension; 
+            $return = $obj->Add();    
+               
+            $filename = $obj->MSGID;
+
+            if(!empty($_FILES ['FileType'] ['name'])) 
+            {
+                if (file_exists(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype)) {
+                    unlink(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype);
+                }
+
+                $fp = fopen(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype, "w");
+                fwrite($fp, fread(fopen($_FILES['FileType']['tmp_name'], 'r'), $_FILES['FileType']['size']));
+                fclose($fp);
+            }
+                        
+        }
+    } 
+    elseif(!empty($obj->MSGID)) 
+    {
+        
+        echo "**222**";
+        die();
+        
+        $size = $_FILES['FileType']['size'];
+
+        if ($size > 2097152) { 
+            echo '<script language="javascript">';
+            echo 'alert("حجم فایل باید کمتراز 2 مگابایت باشد.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        }
+        $st = preg_split("/\./", $_FILES ['FileType'] ['name']);
+
+        $extension = $st [count($st) - 1];
+
+        if (!empty($_FILES ['FileType'] ['name']) && in_array($extension, array("jpg", "jpeg", "gif", "png", "pdf","txt","docx", "JPG", "JPEG", "GIF", "PNG", "PDF","TXT","DOCX")) === false) {           
+            echo '<script language="javascript">';
+            echo 'alert("فرمت فایل غیرمجاز است.")';
+            echo '</script>';
+            echo Response::createObjectiveResponse(false, "");
+            die();
+        } else {
+         
+            $filetype = $obj->FileType = $extension; 
+            $return = $obj->EDIT();    
+               
+            $filename = $obj->MSGID;
+
+            if(!empty($_FILES ['FileType'] ['name'])) 
+            {
+                if (file_exists(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype)) {
+                    unlink(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype);
+                }
+
+                $fp = fopen(GRPMSGPIC_DIRECTORY . $filename . "." . $filetype, "w");
+                fwrite($fp, fread(fopen($_FILES['FileType']['tmp_name'], 'r'), $_FILES['FileType']['size']));
+                fclose($fp);
+            }
+                        
+        }
+    }
+    
+    if (!$return) {
+        echo Response::createObjectiveResponse($return, ExceptionHandler::GetExceptionsToString());
+        die();
+    }
+    echo Response::createObjectiveResponse(true, "");
+    die();
 		
 }
 
+function DelMsg()
+{   
+    
+    //.................. secure section .....................
+    if (!InputValidation::validate($_POST["MsgId"], InputValidation::Pattern_Num, false)) 
+    {
+            echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+            die();
+    }
+   
+    //.......................................................
+
+    $return = manage_msg_messages::Remove($_POST["MsgId"]);
+	Response::createObjectiveResponse($return, (!$return ? ExceptionHandler::popExceptionDescription() : ""));
+	die();
+}
+
+function GetNotNumber() 
+{
+    
+    //.................. secure section .....................
+    if (!InputValidation::validate($_POST["GID"], InputValidation::Pattern_Num, false)) 
+    {
+            echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+            die();
+    }   
+    //.......................................................    
+    $qry = " select me.GID , count(*) MsgNo
+                        from msg_messages me
+                        inner join msg_members mb 
+                               on me.GID = mb.GID
+                        left join msg_messagestatus st
+                               on me.MSGID = st.MSGID and me.MID = st.MID
+                     where mb.PersonID = :PID and me.GID = :GID and mb.MID <> me.MID and  st.MSID is null";
+    $res = PdoDataAccess::runquery($qry, array(":PID" => $_SESSION["USER"]["PersonID"] , ":GID" => $_POST["GID"] ));
+    
+    echo Response::createObjectiveResponse(true,$res[0]['MsgNo']) ;
+    die();
+    
+}
+
+function SeenMsg() 
+{    
+    //.................. secure section .....................
+    if (!InputValidation::validate($_POST["GID"], InputValidation::Pattern_Num, false)) 
+    {
+            echo Response::createObjectiveResponse(false, ExceptionHandler::GetExceptionsToString());
+            die();
+    }   
+    //.......................................................    
+    manage_msg_messages::InsertSeenMsg("", array(":GID" => $_POST["GID"] )); 
+    
+    echo Response::createObjectiveResponse(true," ") ;
+    die();
+    
+}
 
 	
